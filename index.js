1c73af472ac36684338b7551be015f0ba11d28e7
miair3 <gargabayuliberdimurat@gmail.com>
2025-08-05 17:51:03 +0500

    KOki

--- index.js
+++ index.js
@@ -1,46 +1,51 @@
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
-
 const helmet = require('helmet');
 require('dotenv').config();
 const rateLimit = require('express-rate-limit');
+
 const authRoutes = require('./routes/authRoutes');
 const userRoutes = require('./routes/userRoutes');
 const postRouter = require('./routes/postRoutes');
 const likesRouter = require('./routes/likes');
 const gamesRoute = require('./routes/games');
 
-
 const app = express();
-const PORT = 3030;
+const PORT = process.env.PORT || 3030;
 
+// Статические файлы
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
-app.use(cors());
-app.use(helmet());
-app.use('/uploads', express.static('uploads'))
-app.use(express.json());
+// CORS
+app.use(cors({
+  origin: ['http://localhost:5173', 'https://cyberedue.netlify.app'],
+  credentials: true
+}));
 
+// Helmet (с разрешением CORS ресурсов)
+app.use(helmet({
+  crossOriginResourcePolicy: false
+}));
+
+// Ограничение количества запросов
 const limiter = rateLimit({
-  windowMs: 60 * 1000,
+  windowMs: 60 * 1000, // 1 минута
   max: 10,
 });
 app.use(limiter);
 
-app.use(cors({
-    origin: 'http://localhost:5173'
-}))
+// JSON парсер
+app.use(express.json());
 
+// Роуты
 app.use('/api', authRoutes);
 app.use('/api', userRoutes);
 app.use('/api', postRouter);
 app.use('/api/likes', likesRouter);
-
-
 app.use('/games', gamesRoute);
 
-
+// Запуск сервера
 app.listen(PORT, () => {
   console.log(`Сервер работает на http://localhost:${PORT}`);
 });
